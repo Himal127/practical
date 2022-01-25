e7fbd13ad09ef9814ec3935df6366bd2bb5882f2
himalharrier <himal.asawa@harriersys.com>
2022-01-25 14:48:03 +0530

    added code

---
+++ area.py
@@ -0,0 +1,7 @@
+# area of reactangle
+print("entre length of rectangle")
+l = float (input())
+print("enter breadth of rectangle")
+b = float (input())
+area = l * b
+print("area is",area)
---
+++ circle.py
@@ -0,0 +1,6 @@
+print("enter the bradius of circle")
+r = float(input())
+area = 3.14 * r * r
+circumference = 2 * 3.14 * r
+print("area is ", area)
+print("circumference is ",circumference)
